60b5356c053f59c0cde6fd03276552e6ef01c78e
khxrismx <kharisma.supatmono89@myhunter.cuny.edu>
2024-09-25 20:23:13 -0400

    push project

---
+++ script.js
@@ -0,0 +1,71 @@
+let sections = document.querySelectorAll('section');
+let body = document.body;
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+    })
+}
+
+$(window).scroll(function() {
+  
+    // selectors
+    var $window = $(window),
+        $body = $('body'),
+        $panel = $('.panel');
+    
+    // Change 33% earlier than scroll position so colour is there when you arrive.
+    var scroll = $window.scrollTop() + ($window.height() / 3);
+   
+    $panel.each(function () {
+      var $this = $(this);
+      
+      // if position is within range of this panel.
+      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+      // Remember we set the scroll to 33% earlier in scroll var.
+      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+            
+        // Remove all classes on body with color-
+        $body.removeClass(function (index, css) {
+          return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+        });
+         
+        // Add class of currently active div
+        $body.addClass('color-' + $(this).data('color'));
+      }
+    });    
+    
+  }).scroll();
+
+  document.addEventListener("DOMContentLoaded", (event) =>{
+    gsap.to('.img',{
+        duration: 2,
+        y:50,
+        repeat: -1,
+    })
+  });
+/*
+  const lice = document.getElementById('lice');
+  lice.addEventListener('click', () => {
+      const spacingAudio = document.getElementById('spacing');
+      
+      spacingAudio.play().catch((error) => {
+          console.error("Playback failed:", error);
+      });
+  });
+  
+  // Add error listener
+  spacing.addEventListener('error', (e) => {
+      console.error("Error playing audio:", e);
+  });
+  
+  */
